cdc86b2593eac06e7c71aa61e07868b45946f35d
Hezekiah M. Carty <hez@0ok.org>
2026-07-01 23:46:42 -0600

    Automatic formatting, and some casting to clean up behavior on macOS/clang

--- src/Object.cpp
+++ src/Object.cpp
@@ -7,9 +7,9 @@
 
 void Object::OnCreate()
 {
-    PushConfigSection();
-    orxConfig_GetVector("ID", &mIDColor);
-    PopConfigSection();
+  PushConfigSection();
+  orxConfig_GetVector("ID", &mIDColor);
+  PopConfigSection();
 }
 
 void Object::OnDelete()
@@ -22,11 +22,11 @@ void Object::Update(const orxCLOCK_INFO &_rstInfo)
 
 orxBOOL Object::OnShader(orxSHADER_EVENT_PAYLOAD &_rstPayload)
 {
-    if (_rstPayload.eParamType == orxSHADER_PARAM_TYPE_VECTOR && orxString_Compare(_rstPayload.zParamName, "id") == 0)
-    {
-        orxVector_Copy(&_rstPayload.vValue, &mIDColor);
-    }
-    return orxTRUE;
+  if (_rstPayload.eParamType == orxSHADER_PARAM_TYPE_VECTOR && orxString_Compare(_rstPayload.zParamName, "id") == 0)
+  {
+    orxVector_Copy(&_rstPayload.vValue, &mIDColor);
+  }
+  return orxTRUE;
 }
 
 void Label::OnCreate()
@@ -39,36 +39,41 @@ void Label::OnDelete()
 
 void Label::Update(const orxCLOCK_INFO &_rstInfo)
 {
-    orxTEXTURE *pstIDTexture = orxTexture_Get("IDTexture");
-    orxASSERT(pstIDTexture != orxNULL);
+  orxTEXTURE *pstIDTexture = orxTexture_Get("IDTexture");
+  orxASSERT(pstIDTexture != orxNULL);
 
-    orxBITMAP *pstIDBitmap = orxTexture_GetBitmap(pstIDTexture);
-    orxASSERT(pstIDBitmap != orxNULL);
+  orxBITMAP *pstIDBitmap = orxTexture_GetBitmap(pstIDTexture);
+  orxASSERT(pstIDBitmap != orxNULL);
 
-    orxFLOAT fWidth, fHeight;
-    orxASSERT(orxDisplay_GetBitmapSize(pstIDBitmap, &fWidth, &fHeight) == orxSTATUS_SUCCESS);
+  orxFLOAT fWidth, fHeight;
+  orxASSERT(orxDisplay_GetBitmapSize(pstIDBitmap, &fWidth, &fHeight) == orxSTATUS_SUCCESS);
 
-    orxU32 u32Bytes = static_cast<orxU32>(fWidth) * static_cast<orxU32>(fHeight) * 4;
-    orxU8 *au8Buf = (orxU8 *)orxMemory_Allocate(u32Bytes, orxMEMORY_TYPE_TEMP);
-    orxASSERT(au8Buf != orxNULL);
+  orxU32 u32Bytes = static_cast<orxU32>(fWidth) * static_cast<orxU32>(fHeight) * 4;
+  orxU8 *au8Buf = (orxU8 *)orxMemory_Allocate(u32Bytes, orxMEMORY_TYPE_TEMP);
+  orxASSERT(au8Buf != orxNULL);
 
-    orxASSERT(orxDisplay_GetBitmapData(pstIDBitmap, au8Buf, u32Bytes) == orxSTATUS_SUCCESS);
+  orxASSERT(orxDisplay_GetBitmapData(pstIDBitmap, au8Buf, u32Bytes) == orxSTATUS_SUCCESS);
 
-    orxVECTOR vMousePosition = orxVECTOR_0;
-    orxVECTOR vWorldPosition = orxVECTOR_0;
-    orxVIEWPORT *pstViewport = orxViewport_Get("MainViewport");
-    if (orxRender_GetWorldPosition(orxMouse_GetPosition(&vMousePosition), pstViewport, &vWorldPosition) == orxNULL)
-    {
-        // Mouse is outside of the viewport so there isn't anything to update
-        return;
-    }
+  orxVECTOR vMousePosition = orxVECTOR_0;
+  orxVECTOR vWorldPosition = orxVECTOR_0;
+  orxVIEWPORT *pstViewport = orxViewport_Get("MainViewport");
+  if (orxRender_GetWorldPosition(orxMouse_GetPosition(&vMousePosition), pstViewport, &vWorldPosition) == orxNULL)
+  {
+    // Mouse is outside of the viewport so there isn't anything to update
+    return;
+  }
 
-    orxU32 u32Offset = vMousePosition.fX * 4 + vMousePosition.fY * 4 * static_cast<orxU32>(fWidth);
-    orxASSERT(u32Offset < u32Bytes);
+  orxU32 u32Offset = static_cast<orxU32>(vMousePosition.fX) * 4 + static_cast<orxU32>(vMousePosition.fY) * 4 * static_cast<orxU32>(fWidth);
 
-    orxCHAR acLabel[1024];
-    orxString_NPrint(acLabel, sizeof(acLabel), "MOUSE: (%g, %g) LABEL: (%d, %d, %d)", vMousePosition.fX, vMousePosition.fY, au8Buf[u32Offset], au8Buf[u32Offset + 1], au8Buf[u32Offset + 2]);
-    SetText(acLabel);
+  if (u32Offset >= u32Bytes)
+  {
+    // Don't query outside of the texture/bitmap bounds
+    return;
+  }
 
-    orxMemory_Free((void *)au8Buf);
+  orxCHAR acLabel[1024];
+  orxString_NPrint(acLabel, sizeof(acLabel), "MOUSE: (%d, %d) LABEL: (%d, %d, %d)", (int)vMousePosition.fX, (int)vMousePosition.fY, au8Buf[u32Offset], au8Buf[u32Offset + 1], au8Buf[u32Offset + 2]);
+  SetText(acLabel);
+
+  orxMemory_Free((void *)au8Buf);
 }
